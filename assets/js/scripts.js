37c1889293a14aceec93ee8a0676f159ce815671
Adam Maciejewski <adam.m@deligo.pl>
2020-06-16 19:56:43 +0200

    OpanMage page redesign.

---
+++ assets/js/scripts.js
@@ -0,0 +1,51 @@
+$.noConflict();
+
+function getContributorsList() {
+    jQuery.get("https://api.github.com/repos/OpenMage/magento-lts/contributors", function(data, statusTxt, xhr){
+        if(statusTxt == "success") {
+            let contributorsList = '';
+
+            for (x = 0; x < data.length; x++) {
+                if (data[x].avatar_url && data[x].login != 'LeeSaferite' && data[x].login != 'drobinson' && data[x].login != 'Flyingmana' ) {
+                    contributorsList += "<a class='contributor__thumbnail-wrapper' href='" + data[x].html_url + "' target='_blank'><img class='contributor__thumbnail' src='" + data[x].avatar_url + "'/></div>";
+                }
+            }
+
+            jQuery('#js-contributor__thumbnails').append(contributorsList);
+        }
+
+        if(statusTxt == "error") {
+            console.log("Error: " + xhr.status + ": " + xhr.statusText);
+        }
+    });
+}
+
+function getProjectStats() {
+    jQuery.get("https://api.github.com/repos/openmage/magento-lts", function(data, statusTxt, xhr){
+        if(statusTxt == "success") {
+            // Get values
+            const forksCount = data.forks_count;
+            const watchers = data.watchers;
+
+            // Set values on page
+            jQuery('#js-active-info__stats-value--forks').html(forksCount);
+            jQuery('#js-active-info__stats-value--stars').html(watchers);
+        }
+
+        if(statusTxt == "error") {
+            console.log("Error: " + xhr.status + ": " + xhr.statusText);
+        }
+    });
+}
+
+jQuery(document).ready(function( $ ) {
+    setTimeout(function(){
+        $('.js-hero-image-animation').addClass('animate');
+    }, 500);
+
+    getProjectStats();
+});
+
+jQuery(window).on('load', function(){
+    getContributorsList();
+});
